f6f9ce8d7080b64599e0297a4050283712d7d578
Sue M <126427676+suemohandas@users.noreply.github.com>
2023-06-10 17:03:26 -0600

    Initial commit

---
+++ climate_data.py
@@ -0,0 +1,28 @@
+
+def parse_user_input():
+    '''Stage 3'''
+    pass
+
+def dataframe_creation():
+    '''Stage 1 and 2'''
+    pass
+
+def analysis_calculation():
+    '''Stage 4'''
+    pass
+
+def export_data():
+    '''Stage 5 a'''
+    pass
+
+def plot_data():
+    '''Stage 5 b'''
+    pass
+
+
+def main():
+    print("Climate Data")
+
+
+if __name__ == '__main__':
+    main()
